4dad1d90d18e99c4ca602d08760144b2b1093e0a
Gabain AVRIL <gabain.avril@gmail.com>
2023-11-29 16:09:51 +0100

    MP24T2-7 Ajout des attributs et méthodes de la classe Joueur, et des assesseurs

--- Joueur.h
+++ Joueur.h
@@ -1,3 +1,17 @@
+#ifndef JOUEUR_H
+#include <iostream>
+
 class Joueur
 {
+  private:
+    std::string pseudo;
+
+  public:
+    Joueur();
+    ~Joueur();
+    Joueur(const Joueur&);
+
+    std::string getPseudo() const;
+    void        setPseudo(std::string nouveauPseudo);
 };
+#endif
