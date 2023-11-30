c04ae0f2784eb68d154640640a32eea49622b7d7
Gabain AVRIL <gabain.avril@gmail.com>
2023-11-30 08:44:05 +0100

    MP24T2-9 Création des assesseurs et des attributs de la classe Grille

--- Grille.cpp
+++ Grille.cpp
@@ -1,1 +1,23 @@
 #include "Grille.h"
+
+Grille::Grille() : nbLigne(10), nbColonne(10)
+{
+}
+
+Grille::~Grille()
+{
+}
+
+Grille::Grille(const Grille& g) : nbLigne(g.nbLigne), nbColonne(g.nbColonne)
+{
+}
+
+int Grille::getNbColonne() const
+{
+    return this->nbColonne;
+}
+
+int Grille::getNbLigne() const
+{
+    return this->nbLigne;
+}
