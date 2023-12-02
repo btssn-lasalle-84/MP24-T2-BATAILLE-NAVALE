c031246391903c36f803a7a0489ed9054ca197b4
Enzo VALOBRA <valobra.enzo@gmail.com>
2023-12-02 01:20:18 +0100

    Ajout des constructeurs et d'une fonction interface

--- IHM.h
+++ IHM.h
@@ -1,7 +1,15 @@
 #ifndef IHM_H
 #define IHM_H
+#include <vector>
+#include <iostream>
+#include "Flotte.h"
+#include "Grille.h"
+#include "BatailleNavale.h"
 
 class BatailleNavale;
+class Flotte;
+class Grille;
+struct Coordonnees;
 
 class IHM
 {
@@ -9,6 +17,15 @@ class IHM
     BatailleNavale* bataille;
 
   public:
+    IHM();
+    IHM(BatailleNavale*);
+    IHM(const IHM&);
+    ~IHM();
+
+    std::string saisirJoueur();
+    Flotte saisirDisposition();
+    Coordonnees saisirCoup();
+    void afficherGrille(Grille*);
     void associerInterfaceBataille(BatailleNavale*);
 };
 
