545d8955591e2dd1026b67b2029f7af07607cea9
EVALOBRA <valobra.enzo@gmail.com>
2023-12-06 15:39:20 +0100

    MP24T2-29 Ajout de la méthode genererAleatoirement()

--- Flotte.h
+++ Flotte.h
@@ -1,9 +1,15 @@
 #ifndef FLOTTE_H
 #define FLOTTE_H
 #include <vector>
+#include <map>
+#include <stdlib.h>
+#include <time.h>
+#include "Coordonnees.h"
+#include "Navire.h"
 
 class Joueur;
 class Navire;
+class Grille;
 
 class Flotte
 {
@@ -17,6 +23,7 @@ class Flotte
     Flotte(const Flotte&);
     ~Flotte();
 
+    void genererAleatoirement(Grille*);
     void ajouterNavire(Navire*);
     void associerFlotteJoueur(Joueur*);
 };
