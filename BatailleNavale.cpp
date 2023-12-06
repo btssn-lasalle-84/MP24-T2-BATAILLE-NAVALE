e940390b6372de09c6cd81a713c7b3bd12bdb8d8
EVALOBRA <valobra.enzo@gmail.com>
2023-12-06 14:52:59 +0100

    MP24T2-26 Ajout des fonctions pour le démarrage de la partie

--- BatailleNavale.cpp
+++ BatailleNavale.cpp
@@ -1,4 +1,8 @@
 #include "BatailleNavale.h"
+#include "Joueur.h"
+#include "Flotte.h"
+#include "IHM.h"
+#include "Grille.h"
 
 using namespace std;
 
@@ -18,6 +22,51 @@ BatailleNavale::BatailleNavale(const BatailleNavale& bn) :
 
 BatailleNavale::~BatailleNavale()
 {
+}
+
+void BatailleNavale::demarrerPartie()
+{
+    this->initialiserIHM();
+    this->initialiserJoueurs();
+    this->initialiserGrilles();
+    this->initialiserFlottes();
+}
+
+void BatailleNavale::initialiserIHM()
+{
+    IHM ihm;
+    ihm.associerInterfaceBataille(this);
+    this->associerBatailleInterface(&ihm);
+}
+
+void BatailleNavale::initialiserJoueurs()
+{
+    Joueur machine, joueur;
+    machine.setPseudo("Machine");
+    joueur.setPseudo(interface->saisirJoueur());
+    this->associerBatailleJoueurs(&joueur, &machine);
+    machine.associerJoueurBatailleNavale(this);
+    joueur.associerJoueurBatailleNavale(this);
+}
+
+void BatailleNavale::initialiserGrilles()
+{
+    Grille grilleJoueur, grilleJouee, grilleMachine;
+    grilleJoueur.associerGrilleJoueur(joueur1);
+    grilleMachine.associerGrilleJoueur(joueur2);
+    joueur1->associerJoueurGrille(&grilleJoueur);
+    joueur2->associerJoueurGrille(&grilleMachine);
+}
+
+void BatailleNavale::initialiserFlottes()
+{
+    Flotte flotteJoueur, flotteMachine;
+    flotteJoueur = interface->saisirDisposition(joueur1->getGrille());
+    flotteMachine.genererAleatoirement(joueur2->getGrille());
+    flotteJoueur.associerFlotteJoueur(joueur1);
+    flotteMachine.associerFlotteJoueur(joueur2);
+    joueur1->associerJoueurFlotte(&flotteJoueur);
+    joueur2->associerJoueurFlotte(&flotteMachine);
 }
 
 void BatailleNavale::associerBatailleJoueurs(Joueur* j1, Joueur* j2)
