bcf850bea15b96134801d9e2706b985c04cd3ce8
Enzo VALOBRA <valobra.enzo@gmail.com>
2023-12-16 04:50:10 +0100

    MP24T2-41 Révision de méthodes de saisies, amélioration visuelle de l'affichage de la grille

--- IHM.cpp
+++ IHM.cpp
@@ -2,6 +2,8 @@
 #include "Flotte.h"
 #include <iostream>
 #include <map>
+#include <iomanip>
+#include <limits>
 
 using namespace std;
 
@@ -23,51 +25,86 @@ string IHM::saisirJoueur()
 
 bool IHM::estCoupValide(Coordonnees coordonnee, Grille* grille)
 {
-    return !(coordonnee.ligne < 'A' || coordonnee.ligne > 'J' || coordonnee.colonne < 1 ||
-             coordonnee.colonne > NB_COLONNE);
+    return !(coordonnee.ligne < 'A' || coordonnee.colonne < 1 ||
+             coordonnee.ligne > (NB_LIGNE + 'A' - 1) || coordonnee.colonne > NB_COLONNE);
 }
 
 Coordonnees IHM::saisirCoup(Grille* grille)
 {
-    char        delimiteur = '\0';
+    string saisie;
+    getline(cin, saisie);
     Coordonnees coordonnee;
-    cin >> coordonnee.ligne >> delimiteur >> coordonnee.colonne;
-    while(!estCoupValide(coordonnee, grille))
+    if(saisie.size() == 3)
     {
-        cout << "Cette case n'est pas dans la grille !\n";
-        cout << "(Les cases vont de A1 à J10)" << endl;
-        cout << "Saisissez une case valide : ";
-        cin >> coordonnee.ligne >> delimiteur >> coordonnee.colonne;
+        coordonnee.ligne   = char(saisie[0]);
+        coordonnee.colonne = int(char(saisie[2]) - '0');
+    }
+    else if(saisie.size() == 4)
+    {
+        coordonnee.ligne   = char(saisie[0]);
+        coordonnee.colonne = int(char(saisie[2]) - '0');
+        coordonnee.colonne = coordonnee.colonne * 10 + int(char(saisie[3]) - '0');
+    }
+    else
+    {
+        cout << "Saisie invalide, veuillez réessayer :\n";
+        return saisirCoup(grille);
+    }
+
+    if(coordonnee.ligne < 'A' || coordonnee.colonne < 1 || coordonnee.ligne > 'A' + NB_LIGNE - 1 ||
+       coordonnee.colonne > NB_LIGNE)
+    {
+        cout << "Cette case est hors de la grille !" << endl
+             << "Les cases vont de A:1 à " << char('A' - 1 + NB_LIGNE) << ':' << NB_COLONNE << endl;
+        cout << "Saisie invalide, veuillez réessayer :\n";
+        return saisirCoup(grille);
     }
 
     return coordonnee;
+}
+
+Coordonnees IHM::saisirProue(Grille* grille, string nom)
+{
+    cout << "Saisissez la case de proue de votre " << nom << " (ex : A:1) :\n";
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    Coordonnees proue = saisirCoup(grille);
+    return proue;
+}
+
+int IHM::saisirOrientation(Grille* grille, string nom, int nbCases)
+{
+    char orientation;
+    cout << "Saisissez l'orientation de votre " << nom << " (" << nbCases << " cases)\n"
+         << "Horizontal 0 / Vertical 1 : ";
+    cin >> orientation;
+    while(int(orientation - '0') != HORIZONTAL && int(orientation - '0') != VERTICAL)
+    {
+        cout << "Orientation invalide !" << endl;
+        cout << "Saisissez l'orientation de votre " << nom << " (" << nbCases << " cases)\n"
+             << "Horizontal 0 / Vertical 1 : ";
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cin >> orientation;
+    }
+
+    return orientation - '0';
 }
 
 void IHM::saisirDisposition(Grille* grille, Flotte* flotte)
 {
     vector<Navire*> navires;
     vector<string>  nomBateaux     = { "Porte-Avion",
-                                  "Croiseur",
-                                  "Contre-torpilleur",
-                                  "Sous-marin",
-                                  "Torpilleur" };
+                                       "Croiseur",
+                                       "Contre-torpilleur",
+                                       "Sous-marin",
+                                       "Torpilleur" };
     vector<int>     valeursBateaux = { 5, 4, 3, 3, 2 };
 
     for(int i = 0; i < (int)nomBateaux.size(); i++)
     {
-        cout << i << endl;
-        string nom    = nomBateaux[i];
-        int    valeur = valeursBateaux[i];
-
-        cout << "Saisissez l'orientation de votre " << nom << " (" << valeur << " cases)\n"
-             << "Horizontal 0 / Vertical 1 : ";
-        int orientation;
-        cin >> orientation;
-
-        cout << "Saisissez la case de proue de votre " << nom
-             << " sous cette forme : A:1 :" << endl;
-        Coordonnees proue = saisirCoup(grille);
-
+        string                           nom         = nomBateaux[i];
+        int                              valeur      = valeursBateaux[i];
+        int                              orientation = saisirOrientation(grille, nom, valeur);
+        Coordonnees                      proue       = saisirProue(grille, nom);
         vector<pair<Coordonnees, bool> > coordonnees;
         Navire                           navire(nom, orientation, coordonnees);
 
@@ -76,43 +113,32 @@ void IHM::saisirDisposition(Grille* grille, Flotte* flotte)
         bool navireInvalide = true;
         while(navireInvalide)
         {
+            navire.setOrientation(orientation);
             coordonnees.clear();
 
-            if(orientation == HORIZONTAL)
+            for(int j = 0; j < valeur; ++j)
             {
-                for(int j = 0; j < valeur; ++j)
-                {
-                    pair<Coordonnees, bool> coordonnee;
-                    coordonnee.first.colonne = proue.colonne + j;
-                    coordonnee.first.ligne   = proue.ligne;
-                    coordonnee.second        = true;
-                    coordonnees.push_back(coordonnee);
-                }
-            }
-            else
-            {
-                for(int j = 0; j < valeur; ++j)
-                {
-                    pair<Coordonnees, bool> coordonnee;
-                    coordonnee.first.colonne = proue.colonne;
-                    coordonnee.first.ligne   = proue.ligne + j;
-                    coordonnee.second        = true;
-                    coordonnees.push_back(coordonnee);
-                }
+                pair<Coordonnees, bool> coordonnee;
+                coordonnee.first.colonne = proue.colonne + (j * (1 - orientation));
+                coordonnee.first.ligne   = proue.ligne + (j * orientation);
+                coordonnee.second        = true;
+                coordonnees.push_back(coordonnee);
             }
 
             navire.setCoordonnees(coordonnees);
 
-            if(navire.estNavireValide(grille, navires, navire))
+            if(navire.estNavireValide(grille, navires))
             {
                 navireInvalide = false;
+                cout << endl;
                 navires.push_back(new Navire(navire)); // Copie le navire dans le vecteur
                 cout << "Navire ajouté" << endl;
             }
             else
             {
                 cout << "Navire invalide, veuillez le replacer." << endl;
-                Coordonnees proue = saisirCoup(grille);
+                orientation = saisirOrientation(grille, nom, valeur);
+                proue       = saisirProue(grille, nom);
             }
         }
     }
@@ -131,45 +157,53 @@ void IHM::afficherGrille(Joueur* joueur)
 {
     vector<vector<string> > matrice(joueur->getGrille()->getNbLignes(),
                                     vector<string>(joueur->getGrille()->getNbColonnes(), ""));
+
+    for(int i = 0; i < joueur->getGrille()->getNbLignes(); ++i)
+    {
+        for(int j = 0; j < joueur->getGrille()->getNbColonnes(); ++j)
+        {
+            if((j + i) % 2 == 0)
+            {
+                matrice[i][j] = BLEU;
+            }
+            else if((j + i) % 2 == 1)
+            {
+                matrice[i][j] = CYAN;
+            }
+        }
+    }
+
     for(Navire* navire: joueur->getFlotte()->getFlotte())
     {
         for(pair<Coordonnees, bool> coordonnee: navire->getCoordonnes())
         {
             if(coordonnee.second)
             {
-                matrice[coordonnee.first.ligne - 'A'][coordonnee.first.colonne] = GRIS;
+                matrice[coordonnee.first.ligne - 'A'][coordonnee.first.colonne - 1] = GRIS;
             }
             else
             {
-                matrice[coordonnee.first.ligne - 'A'][coordonnee.first.colonne] = ROUGE;
+                matrice[coordonnee.first.ligne - 'A'][coordonnee.first.colonne - 1] = ROUGE;
             }
         }
     }
 
-    for(int i = 0; i < joueur->getGrille()->getNbLignes(); ++i)
+    cout << endl << "Grille de : " << joueur->getPseudo() << endl << endl;
 
+    cout << DEFAUT << "X 1 2 3 4 5 6 7 8 9 10" << endl;
+    for(size_t i = 0; i < matrice.size(); ++i)
     {
-        for(int j = 0; j < joueur->getGrille()->getNbColonnes(); ++j)
-        {
-            if((j + i) % 2 == 0 && matrice[i][j] == "")
-            {
-                matrice[i][j] == BLEU;
-            }
-            else if((j + i) % 2 == 1 && matrice[i][j] == "")
-            {
-                matrice[i][j] == CYAN;
-            }
-        }
-    }
-
-    for(vector<string> i: matrice)
-    {
-        for(string j: i)
+        cout << DEFAUT << char(i + 'A') << ' ';
+        for(string j: matrice[i])
         {
             cout << j;
         }
-        cout << endl;
+        cout << DEFAUT << endl;
     }
+
+    cout << DEFAUT << endl
+         << "Navires restants : " << joueur->getFlotte()->getFlotte().size() << endl
+         << endl;
 }
 
 void IHM::afficherAsciiArt()
