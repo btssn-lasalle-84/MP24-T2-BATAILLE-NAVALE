2001867b8e7b6ecfea5cc1babbfe67677d72f7cf
Gabain AVRIL <gabain.avril@gmail.com>
2023-12-21 14:43:56 +0100

    MP24T2-47 Ajout de la fonction AfficherVersion

--- IHM.h
+++ IHM.h
@@ -9,12 +9,14 @@
 #include "Navire.h"
 #include "Joueur.h"
 
+#define VERSION    1.1
+#define ROSE       "\033[1;38;5;200m"
 #define BLEU       "\033[48;5;33m  "
 #define CYAN       "\033[48;5;75m  "
 #define ROUGE      "\033[48;5;196m  "
 #define GRIS       "\033[48;5;236m  "
 #define BLEU_REGLE "\033[38;5;153m"
-#define VERT       "\033[1;38;5;46m  "
+#define VERT       "\033[1;38;5;46m"
 #define DEFAUT     "\033[0;5;0m"
 #define NB_LIGNE   10
 #define NB_COLONNE 10
@@ -50,6 +52,7 @@ class IHM
     static void afficherAsciiArt();
     void        afficherRegles();
     std::string afficherGrilleVierge();
+    void        afficherVersionLogiciel();
 };
 
 #endif
