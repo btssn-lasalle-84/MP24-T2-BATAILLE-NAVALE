6d7e2ddd05392982ae6bdaa3493bf01aaa8aa816
Enzo VALOBRA <valobra.enzo@gmail.com>
2024-01-06 11:37:49 +0100

    MP24T2-49 Prise en compte des caractères minuscules à la saisie

--- IHM.h
+++ IHM.h
@@ -9,17 +9,19 @@
 #include "Navire.h"
 #include "Joueur.h"
 
-#define VERSION    1.1
-#define ROSE       "\033[1;38;5;200m"
-#define BLEU       "\033[48;5;33m  "
-#define CYAN       "\033[48;5;75m  "
-#define ROUGE      "\033[48;5;196m  "
-#define GRIS       "\033[48;5;236m  "
-#define BLEU_REGLE "\033[38;5;153m"
-#define VERT       "\033[1;38;5;46m"
-#define DEFAUT     "\033[0;5;0m"
-#define NB_LIGNE   10
-#define NB_COLONNE 10
+#define VERSION         1.1
+#define ROSE            "\033[1;38;5;200m"
+#define BLEU            "\033[48;5;33m  "
+#define CYAN            "\033[48;5;75m  "
+#define ROUGE           "\033[48;5;196m  "
+#define GRIS            "\033[48;5;236m  "
+#define BLEU_REGLE      "\033[38;5;153m"
+#define VERT            "\033[1;38;5;46m"
+#define DEFAUT          "\033[0;5;0m"
+#define NB_LIGNE        10
+#define NB_COLONNE      10
+#define TAILLE_SAISIE_1 3
+#define TAILLE_SAISIE_2 4
 
 class BatailleNavale;
 class Flotte;
@@ -41,10 +43,10 @@ class IHM
 
     std::string saisirJoueur();
     void        saisirDisposition(Grille*, Flotte* flotte);
-    Coordonnees saisirCoup(Grille*);
-    Coordonnees saisirProue(Grille*, std::string);
-    int         saisirOrientation(Grille*, std::string, int);
-    bool        estCoupValide(Coordonnees, Grille*);
+    Coordonnees saisirCoup();
+    Coordonnees saisirProue(std::string);
+    int         saisirOrientation(std::string, int);
+    bool        estCoupValide(Coordonnees);
     void        afficherGrille(Joueur*);
     void        afficherNavire(Grille*, Flotte*);
     void        associerInterfaceBataille(BatailleNavale*);
